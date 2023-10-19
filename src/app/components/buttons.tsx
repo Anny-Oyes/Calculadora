46772c3675c94b2067555edd74c1d58a9c7d3931
Anny-Oyes <stephanyoyes@gmail.com>
2023-10-18 18:08:31 -0600

    Mejorando props

--- src/app/components/buttons.tsx
+++ src/app/components/buttons.tsx
@@ -6,18 +6,9 @@ export default function Buttons({ texto, handleKeyPress, color, size }: { texto:
 
     const buttonColor = `${color} text-white font-bold py-4 hover:border-zinc-950`;
 
-    const buttonSize: any = {
-        small: 'text-lg w-1/4',
-        smaller: 'text-lg w-1/4 rounded-br-lg',
-        medium: "text-lg w-2/4 px-12 rounded-bl-lg",
-        large: 'text-lg w-3/4 px-24',
-    };
-
-    const sizeList = buttonSize[size] || '';
-
     return (
         <div
-            className={`${buttonColor} ${sizeList} border border-slate-800`}
+            className={`${buttonColor} ${size} border border-slate-800`}
             onClick={() => handleKeyPress(texto)}
         >
             <div className="h-12 font-bold text-3xl flex flex-col justify-center text-center text-white">
--- src/app/components/keyboard.tsx
+++ src/app/components/keyboard.tsx
@@ -10,32 +10,32 @@ export default function Keyboard({ keyPress }: { keyPress?: any }) {
     return (
         <div className="mt-5">
             <div className="flex flex-row justify-between">
-                <Buttons color="bg-gray-700" size="large" texto="C" handleKeyPress={sendKey} />
-                <Buttons color="bg-orange-400" size="small" texto="÷" handleKeyPress={sendKey} />
+                <Buttons color="bg-gray-700" size="text-lg w-3/4 px-24" texto="C" handleKeyPress={sendKey} />
+                <Buttons color="bg-orange-400" size="text-lg w-1/4" texto="÷" handleKeyPress={sendKey} />
             </div>
             <div className="flex flex-row justify-between">
-                <Buttons color="bg-gray-600" size="small" texto="7" handleKeyPress={sendKey} />
-                <Buttons color="bg-gray-600" size="small" texto="8" handleKeyPress={sendKey} />
-                <Buttons color="bg-gray-600" size="small" texto="9" handleKeyPress={sendKey} />
-                <Buttons color="bg-orange-400" size="small" texto="*" handleKeyPress={sendKey} />
+                <Buttons color="bg-gray-600" size="text-lg w-1/4" texto="7" handleKeyPress={sendKey} />
+                <Buttons color="bg-gray-600" size="text-lg w-1/4" texto="8" handleKeyPress={sendKey} />
+                <Buttons color="bg-gray-600" size="text-lg w-1/4" texto="9" handleKeyPress={sendKey} />
+                <Buttons color="bg-orange-400" size="text-lg w-1/4" texto="*" handleKeyPress={sendKey} />
 
             </div>
             <div className="flex flex-row justify-between">
-                <Buttons color="bg-gray-600" size="small" texto="4" handleKeyPress={sendKey} />
-                <Buttons color="bg-gray-600" size="small" texto="5" handleKeyPress={sendKey} />
-                <Buttons color="bg-gray-600" size="small" texto="6" handleKeyPress={sendKey} />
-                <Buttons color="bg-orange-400" size="small" texto="-" handleKeyPress={sendKey} />
+                <Buttons color="bg-gray-600" size="text-lg w-1/4" texto="4" handleKeyPress={sendKey} />
+                <Buttons color="bg-gray-600" size="text-lg w-1/4" texto="5" handleKeyPress={sendKey} />
+                <Buttons color="bg-gray-600" size="text-lg w-1/4" texto="6" handleKeyPress={sendKey} />
+                <Buttons color="bg-orange-400" size="text-lg w-1/4" texto="-" handleKeyPress={sendKey} />
             </div>
             <div className="flex flex-row justify-between">
-                <Buttons color="bg-gray-600" size="small" texto="1" handleKeyPress={sendKey} />
-                <Buttons color="bg-gray-600" size="small" texto="2" handleKeyPress={sendKey} />
-                <Buttons color="bg-gray-600" size="small" texto="3" handleKeyPress={sendKey} />
-                <Buttons color="bg-orange-400" size="small" texto="+" handleKeyPress={sendKey} />
+                <Buttons color="bg-gray-600" size="text-lg w-1/4" texto="1" handleKeyPress={sendKey} />
+                <Buttons color="bg-gray-600" size="text-lg w-1/4" texto="2" handleKeyPress={sendKey} />
+                <Buttons color="bg-gray-600" size="text-lg w-1/4" texto="3" handleKeyPress={sendKey} />
+                <Buttons color="bg-orange-400" size="text-lg w-1/4" texto="+" handleKeyPress={sendKey} />
             </div>
             <div className="flex flex-row justify-between">
-                <Buttons color="bg-gray-600" size="medium" texto="0" handleKeyPress={sendKey} />
-                <Buttons color="bg-gray-600" size="small" texto="." handleKeyPress={sendKey} />
-                <Buttons color="bg-orange-400" size="smaller" texto="=" handleKeyPress={sendKey} />
+                <Buttons color="bg-gray-600" size="text-lg w-2/4 px-12 rounded-bl-lg" texto="0" handleKeyPress={sendKey} />
+                <Buttons color="bg-gray-600" size="text-lg w-1/4" texto="." handleKeyPress={sendKey} />
+                <Buttons color="bg-orange-400" size="text-lg w-1/4 rounded-br-lg" texto="=" handleKeyPress={sendKey} />
             </div>
         </div>
     )
