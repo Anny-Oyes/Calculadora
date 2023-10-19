b32bdd16da7fce97983d38d7ff044714f7e70b99
Anny-Oyes <stephanyoyes@gmail.com>
2023-10-18 18:12:05 -0600

    Diseño final

--- src/app/components/buttons.tsx
+++ src/app/components/buttons.tsx
@@ -4,11 +4,20 @@ import React from 'react';
 
 export default function Buttons({ texto, handleKeyPress, color, size }: { texto: string, handleKeyPress?: any, color: string, size: string }) {
 
-    const buttonColor = `${color}text-white font-bold py-4 hover:border-zinc-950`;
+    const buttonColor = `${color} text-white font-bold py-4 hover:border-zinc-950`;
+
+    const buttonSize: any = {
+        small: 'text-lg w-1/4',
+        smaller: 'text-lg w-1/4 rounded-br-lg',
+        medium: "text-lg w-2/4 px-12 rounded-bl-lg",
+        large: 'text-lg w-3/4 px-24',
+    };
+
+    const sizeList = buttonSize[size] || '';
 
     return (
         <div
-            className={`${buttonColor} ${size} border border-slate-800`}
+            className={`${buttonColor} ${sizeList} border border-slate-800`}
             onClick={() => handleKeyPress(texto)}
         >
             <div className="h-12 font-bold text-3xl flex flex-col justify-center text-center text-white">
